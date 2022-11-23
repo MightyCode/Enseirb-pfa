a3e44ca16dbc2bd4da0ad36cd628755cd027620b
MightyCode <bazinmaxence@free.fr>
2022-11-23 12:45:45 +0100

    add tweening for js

---
+++ libraries/js/Tweenings/tweening_const.js
@@ -0,0 +1,19 @@
+const ETweeningType = {
+    LINEAR : 0,
+    QUADRATIC : 1,
+    CUBIC : 2,
+    QUARTIC : 3,
+    QUINTIC : 4,
+    SINUSOIDAL : 5,
+    EXPONENTIAL : 6,
+    CIRCULAR : 7,
+    ELASTIC : 8,
+    BACK : 9,
+    BOUNCE : 10
+};
+
+const ETweeningBehaviour = {
+    IN : 0,
+    OUT : 1,
+    IN_OUT : 2
+};
