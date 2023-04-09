a480790329923f4ac7c72c036c5b5189988403ab
Matteo Caravati <matteo.caravati@protonmail.com>
2023-04-09 15:23:33 +0200

    (feat) Added infobar

--- web/interface/src/App.vue
+++ web/interface/src/App.vue
@@ -1,17 +1,60 @@
 <template>
     <div id="main">
       <Header />
+
+      <div class="infobar">
+        <div class="infobar-item">
+          <span>Config: {{ activeConfig ? `${activeConfig.id} (${activeConfig.nbLights} lights, ${activeConfig.nbSpeakers} speakers)` : 'None' }}</span>
+        </div>
+
+        <div class="infobar-item">
+          <span>Project: {{ activeProject ? activeProject.id : 'None' }}</span>
+        </div>
+      </div>
+
       <div>
         <router-view />
       </div>
     </div>
 </template>
 
-<script setup>
+<script>
 import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header
+  },
+  computed: {
+    activeProject() {
+      return this.$store.state.activeProject;
+    },
+
+    activeConfig() {
+      return this.$store.state.activeConfig;
+    }
+  }
+}
 </script>
 
 <style scoped>
+.infobar {
+  height: 1.7em;
+  width: 100%;
+
+  display: flex;
+}
+
+.infobar-item {
+  background-color: #7b7a7a;
+  height: 100%;
+  width: fit-content;
+  padding: 0 1em;
+  border-radius: 4px;
+  margin-right: 1em;
+}
+
 #main {
   width: 100%;
   height: 100%;
@@ -21,9 +64,9 @@ import Header from './components/Header.vue'
   justify-content: space-between;
 }
 
-#main > div {
+#main > div:last-of-type {
   background-color: #7b7a7a;
-  height: 88%;
+  height: 85%;
   border-radius: 7px;
   padding: 1%;
 }
